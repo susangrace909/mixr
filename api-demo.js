70606a1b26e8bcb768d3bb6ea2dc750165cc64e9
Ric <cnidarAI@gmail.com>
2023-01-21 11:42:57 -0500

    add name and img

--- api-demo.js
+++ api-demo.js
@@ -5,6 +5,11 @@ function getRandomDrink(){
         .then(res => res.json()) // parse response as JSON
         .then(data => {
             console.log(data.drinks);
+            let cocktailName = data.drinks[0].strDrink;
+            // console.log(cocktailName);
+            document.getElementById('cocktailName').innerText = cocktailName;
+            let cocktailImgSrc = data.drinks[0].strDrinkThumb
+            document.getElementById('cocktailImg').setAttribute('src', cocktailImgSrc);
         })
         .catch(err => {
             console.log(`error ${err}`)
