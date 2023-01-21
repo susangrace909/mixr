1b274433c1222b6eec10a53eaf5f62bbf684c5dc
Ric <cnidarAI@gmail.com>
2023-01-21 14:00:57 -0500

    messing with drinklist

--- frontend/src/api-demo.js
+++ frontend/src/api-demo.js
@@ -1,28 +1,29 @@
-function getRandomDrink(){
+export default function getRandomDrink(){
  
     let url = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
     fetch(url)
         .then(res => res.json()) // parse response as JSON
         .then(data => {
             if(data.drinks[0].strAlcoholic != 'Alcoholic') getRandomDrink();
-            console.log(data.drinks);
+            // console.log(data.drinks);
             let cocktailName = data.drinks[0].strDrink;
             // console.log(cocktailName);
-            document.getElementById('cocktailName').innerText = cocktailName;
+            // document.getElementById('cocktailName').innerText = cocktailName;
             let cocktailImgSrc = data.drinks[0].strDrinkThumb
-            document.getElementById('cocktailImg').setAttribute('src', cocktailImgSrc);
+            // document.getElementById('cocktailImg').setAttribute('src', cocktailImgSrc);
 
 
             let ingredients = Object.entries(data.drinks[0]).filter(x => x[0].includes('Ingredient'));
             ingredients = ingredients.filter(x => x[1]);
             let ingredientsArr = ingredients.map(x => x[1]);
             // console.log(ingredients);
-            console.log(ingredientsArr);
-            let ingredientsList = document.getElementById('ingredients');
-            ingredientsArr.forEach(ingredient => {
-                let ingredientItem = ingredientsList.appendChild(document.createElement('li'));
-                ingredientItem.innerText = ingredient;
-            });
+            // console.log(ingredientsArr);
+            // let ingredientsList = document.getElementById('ingredients');
+            // ingredientsArr.forEach(ingredient => {
+            //     let ingredientItem = ingredientsList.appendChild(document.createElement('li'));
+            //     ingredientItem.innerText = ingredient;
+            // });
+            return([data.drinks[0], cocktailName, cocktailImgSrc, ingredientsArr]);
         })
         .catch(err => {
             console.log(`error ${err}`)
