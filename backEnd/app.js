f5217355ea0bd2738ed1c39d6f809c8fb7ba036d
germangamboa95 <germangamboa95@gmail.com>
2023-01-21 11:56:12 -0500

    add stuff

--- backEnd/app.js
+++ backEnd/app.js
@@ -2,8 +2,32 @@ const express = require("express");
 
 const app = express();
 
+const drinks = [];
+
 app.get("/", (req, res) => {
   return res.send("<h1>Hello World</h1> ");
 });
 
+app.get("/orders/drinks", (req, res) => {
+  return res.json({
+    drinks,
+  });
+});
+
+app.post("/orders/drinks", (req, res) => {
+  const drink = req.body.drink;
+
+  drinks.push(drink);
+
+  return res.json({
+    drink,
+  });
+});
+
+app.delete("/orders/drinks/{drink_id}", (req, res) => {
+  const drink = req.params.drink_id;
+
+  // Delete drink logic
+  return res.send();
+});
 app.listen(3000);
